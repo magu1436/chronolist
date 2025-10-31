67580124c86897b81e69662f0f6bbc93eceb05ea
magu1436 <hfzoro77@gmail.com>
2025-10-31 21:07:16 +0900

    `axios.tsx` に環境変数が見つからなかった際にエラーを投げるように変更

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,6 +1,7 @@
 import Axios from "axios";
 
 const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL;
+if (!API_BASE_URL) throw new Error('Not found the environment variable: "VITE_API_BASE_URL". Check the value of "env" in "launch.json".');
 
 const axios = Axios.create({
     baseURL: API_BASE_URL + "app/",
