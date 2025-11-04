de021011bbb88441992b2846709e1bb10742362b
magu1436 <hfzoro77@gmail.com>
2025-11-04 12:06:11 +0900

    特定期間のカレンダーイベント取得APIのテスターの実装.

--- frontend/src/feature/tester/components/scheduler/getEvents.tsx
+++ frontend/src/feature/tester/components/scheduler/getEvents.tsx
@@ -1,5 +1,8 @@
+import type { CalendarEventApi } from "@/feature/schesuler/types/api";
 import useFetch from "@/utils/fetch";
-import { useMemo } from "react"
+import { useMemo, type FC } from "react"
+import ErrorDisplay from "../error";
+import classNames from "classnames";
 
 
 const GetEventsResults = () => {
@@ -14,4 +17,67 @@ const GetEventsResults = () => {
         };
     }, []);
 
-}
+    const {data, isLoading, error} = useFetch<CalendarEventApi[]>(
+        "scheduler/getEvents",
+        "GET",
+        body,
+    );
+
+    if (isLoading) return (
+        <h1>Loading...</h1>
+    );
+
+    if (error) return (
+        <>
+            <Body {...body} />
+            <ErrorDisplay err={error} />
+        </>
+    );
+
+    return (
+        <>
+            <Body {...body} />
+            <h1>Completed!</h1>
+            <div className={classNames("d-flex", "flex-column")}>
+                {data? data.map(ce => <CE {...ce} />): "No Data"}
+            </div>
+        </>
+    )
+
+};
+
+const Body: FC<{start: string, end: string}> = ({start, end}) => {
+    return (
+        <>
+            <h1>Submitted data</h1>
+            <div>start: {start}</div>
+            <div>end: {end}</div>
+        </>
+    )
+};
+
+const CE: FC<CalendarEventApi> = ({
+    id,
+    kind,
+    startAt,
+    endAt,
+    startDate,
+    endDate,
+    title,
+    color,
+    memo,
+}) => {
+    return (
+        <div className={classNames("m-2", "border", "border-2", "border-blue", "d-flex", "flex-column")}>
+            <div>id: {id}</div>
+            <div>kind: {kind}</div>
+            <div>startAt: {startAt || "null"}</div>
+            <div>endAt: {endAt || "null"}</div>
+            <div>startDate: {startDate || "null"}</div>
+            <div>endDate: {endDate || "null"}</div>
+            <div>title: {title}</div>
+            <div>color: {color}</div>
+            <div>memo: {memo || "null"}</div>
+        </div>
+    );
+};
