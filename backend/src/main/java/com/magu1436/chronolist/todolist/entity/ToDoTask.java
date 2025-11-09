4aa608e03aa4bc8754c51ed83c8d172c7aa48160
milk0924 <sjbt8531zkysyms0pb4w@gmail.com>
2025-11-09 23:33:06 +0900

    クラス名の修正

--- backend/src/main/java/com/magu1436/chronolist/todolist/entity/ToDoTask.java
+++ backend/src/main/java/com/magu1436/chronolist/todolist/entity/ToDoTask.java
@@ -1,6 +1,7 @@
 package com.magu1436.chronolist.todolist.entity;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,9 +18,9 @@ public class ToDoTask {
     /** 期日の設定 */
     private DueKind dueKind;
     /** dateの設定 */
-    private LocalDate Date;
+    private LocalDate dueDate;
     /** timeの設定 */
-    private LocalDate Time;
+    private LocalTime dueTime;
     /** 優先度の設定 
      * StringはPriorityクラスの定義後置き換え
     */
