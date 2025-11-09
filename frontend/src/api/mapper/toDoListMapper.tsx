10a0edf81e59a80ac41dd64b56d219936075e94a
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-11-09 22:19:22 +0900

    `Due` の撤廃に合わせて, `toToDoTask` マッパーの返り値を変更

--- frontend/src/api/mapper/toDoListMapper.tsx
+++ frontend/src/api/mapper/toDoListMapper.tsx
@@ -1,13 +1,14 @@
 import type { GetAllApi } from "@/types/todolist/api";
 import type { ToDoTask } from "@/types/todolist/todotask";
-import Due from "@/utils/todolist/due";
 
 
 const toToDoTask = (apiObj: GetAllApi): ToDoTask => {
     return {
         id: apiObj.id,
         title: apiObj.title,
-        due: Due.createFromDueApi(apiObj.due),
+        dueKind: apiObj.dueKind,
+        date: apiObj.date,
+        time: apiObj.time,
         priority: apiObj.priority,
         isCompleted: apiObj.isCompleted,
         memo: apiObj.memo || undefined,
