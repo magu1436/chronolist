61016241c55f784bc48fba4052e24cdda842b4e1
milk0924 <sjbt8531zkysyms0pb4w@gmail.com>
2025-11-09 23:35:11 +0900

    getallメソッドの実装

---
+++ backend/src/main/java/com/magu1436/chronolist/todolist/ToDoListController.java
@@ -0,0 +1,31 @@
+package com.magu1436.chronolist.todolist;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.magu1436.chronolist.todolist.entity.ToDoTask;
+import com.magu1436.chronolist.todolist.mapper.ToDoMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("api/todolist/")
+@Controller
+@RequiredArgsConstructor
+public class ToDoListController {
+    
+    private final ToDoMapper mapper;
+
+    @GetMapping("getAll")
+    public ResponseEntity<List<ToDoTask>> getall(){
+        List<ToDoTask> tasks = mapper.getAllTasks();
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
+}
