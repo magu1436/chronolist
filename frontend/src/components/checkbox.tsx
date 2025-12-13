2c724de42962b132f5f8939893483c203e638afe
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-12-13 13:15:36 +0900

    汎用的なチェックボックスを実装

---
+++ frontend/src/components/checkbox.tsx
@@ -0,0 +1,35 @@
+import { useState, type ChangeEvent, type FC } from "react"
+
+
+type CheckBoxProps = {
+    id?: string,
+    className?: string,
+    defaultChecked?: boolean,
+    onChange?: (isChecked: boolean) => void,
+}
+
+const CheckBox: FC<CheckBoxProps> = ({
+    id,
+    className,
+    defaultChecked = false,
+    onChange,
+}) => {
+    const [isChecked, setIsChecked] = useState<boolean>(defaultChecked);
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const newChecked = e.target.checked;
+        setIsChecked(newChecked);
+        onChange?.(newChecked);
+    };
+
+    return (
+        <input
+            type="checkbox"
+            id={id}
+            className={className}
+            checked={isChecked}
+            onChange={handleChange}
+        />
+    );
+}
+
+export default CheckBox;
