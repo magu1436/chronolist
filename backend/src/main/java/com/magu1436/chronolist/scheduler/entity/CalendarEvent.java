c772132f3134bbf7c2b6a461f72a43e5be45c67a
konoma1103 <2CDI1103.tokai@gmail.com>
2025-12-20 16:03:45 +0900

    CalendarEventエンティティにscheduleIdを追加

--- backend/src/main/java/com/magu1436/chronolist/scheduler/entity/CalendarEvent.java
+++ backend/src/main/java/com/magu1436/chronolist/scheduler/entity/CalendarEvent.java
@@ -11,6 +11,8 @@ import lombok.NoArgsConstructor;
 public class CalendarEvent {
     /** カレンダーイベントID */
     private int id;
+    /** スケジュールID */
+    private int scheduleId;
     /** スケジュールの種類 */
     private ScheduleKind kind;
     /** 開始時刻 */
