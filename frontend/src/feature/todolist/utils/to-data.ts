df5c40a822eac5e5cd8b089e5c928fa628548138
magu1436 <hfzoro77@gmail.com>
2025-12-30 01:32:39 +0900

    実装した関数にドックコメントを追加

--- frontend/src/feature/todolist/utils/to-data.ts
+++ frontend/src/feature/todolist/utils/to-data.ts
@@ -1,5 +1,12 @@
 import type { Priority } from "@/types/todolist/statics";
 
+/**
+ * 優先度の表示テキストを内部データ上の文字列に変換する。
+ * 
+ * @param displayText 優先度の表示用テキスト
+ * @returns 内部データ上の文字列
+ * @throws 変換できない文字列を受け取った場合に生じる
+ */
 export const convertDisplayTextToPriority = (displayText: string): Priority => {
     switch (displayText) {
         case "高":
