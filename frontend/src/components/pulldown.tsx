764413d589efd76d4132b4ed35fa97e43ed087d4
magu1436 <hfzoro77@gmail.com>
2025-12-30 03:40:41 +0900

    プルダウンが表示テキストと値を異なるデータとして扱えるように仕様を変更

--- frontend/src/components/pulldown.tsx
+++ frontend/src/components/pulldown.tsx
@@ -1,6 +1,6 @@
 import type { PullDownProps } from "@/types/utility-prop";
 import classNames from "classnames";
-import type { FC } from "react";
+import { useCallback, useState, type FC } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 // プルダウン要素固有のクラス名
@@ -15,35 +15,48 @@ const PULLDOWN_CLASS_NAMES = classNames(
 /**
  * Chronolist共通のプルダウン要素を提供するコンポーネント。  
  * 
- * デフォルト値が指定されていない場合、 `items` の最初のアイテムを  
+ * `values` が設定されていない場合、 `items` をそのまま使用する。  
+ * 
+ * デフォルト値が指定されていない場合、 `values` の最初のアイテムを  
  * デフォルト値として使用する。  
  * 
- * デフォルト値を指定した場合に、その値が `items` に含まれていない場合、  
+ * デフォルト値を指定した場合に、その値が `values` に含まれていない場合、  
  * コンソールに警告を出し、最初のアイテムをデフォルト値として使用する。  
  * 
+ * `onChange` の引数には、 `values` の値が渡されることに注意。  
+ * 
  * @throws プルダウンに含まれるアイテムが0個の場合に発生する。
+ * @throws `items` と `values` の長さが異なる場合に発生する。
  */
-const PullDown: FC<PullDownProps> = ({ items, onChange, defaultValue, formLabel }) => {
+const PullDown: FC<PullDownProps> = ({ items, onChange, defaultValue, formLabel, values: originalValues }) => {
 
     if (items.length === 0) throw new Error("No items were given to PullDown");
 
-    let defaultSelectedValue = defaultValue || items[0];
+    const [ values, ] = useState(originalValues || items);
+
+    if (values.length !== items.length) throw new Error("[chronolist] The number of values must be the same as the number of items");
+
+    const [ defaultSelectedValue, setDefaultSelectedValue] = useState(defaultValue || values[0]);
 
     // デフォルト値が指定されている場合に、その値がitemsに含まれているかどうかのチェック
-    if (defaultValue && !items.includes(defaultSelectedValue)) {
-        console.log(`[chronolist]WARNING: The default value "${defaultSelectedValue}" is not included in items. The first item "${items[0]}" will be used instead.`);
-        defaultSelectedValue = items[0];
+    if (defaultValue && !values.includes(defaultSelectedValue)) {
+        console.log(`[chronolist]WARNING: The default value "${defaultSelectedValue}" is not included in items. The first value "${values[0]}" will be used instead.`);
+        setDefaultSelectedValue(values[0]);
     }
+
+    const handleSelect = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
+        onChange?.(event.target.value);
+    }, []);
 
     return (
         <select
             className={PULLDOWN_CLASS_NAMES}
-            onChange={(e) => {onChange(e.target.value)}}
+            onChange={handleSelect}
             aria-label={formLabel || uuidv4()}
         >
-            {items.map((item) => (
-                <option key={item} value={item} selected={item === defaultSelectedValue}>
-                    {item}
+            {Array.from({ length: items.length }, (_, i) => i).map((i) => (
+                <option key={i} value={values[i]} defaultValue={defaultSelectedValue}>
+                    {items[i]}
                 </option>
             ))}
         </select>
--- frontend/src/types/utility-prop.tsx
+++ frontend/src/types/utility-prop.tsx
@@ -1,8 +1,32 @@
 
 
 export interface PullDownProps {
+    /**
+     * プルダウンに表示される文字列
+     */
     items: string[];
-    onChange: (item: string) => void | null;
+
+    /**
+     * 文字列が選択された際に使用するデータ上の値。  
+     * 指定がない場合は `items` を代わりに使用する。
+     */
+    values?: string[];
+
+    /**
+     * 選択時に実行する関数
+     * @param value 選択された項目の値
+     */
+    onChange?: (value: string) => void;
+
+    /**
+     * デフォルト値。 `values` 内の値を指定する。  
+     * 指定されていない場合、 `items` の最初の項目を使用する。
+     */
     defaultValue?: string;
+
+    /**
+     * フォームとして使用する場合の識別子。  
+     * 指定がない場合、自動でUUIDが生成される。
+     */
     formLabel?: string;
 }
