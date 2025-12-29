77eab90fb3721517804c2df793ab7d095877b00b
magu1436 <hfzoro77@gmail.com>
2025-12-30 01:24:41 +0900

    `Priority` を列挙型で定義し、イテレータとして扱えるように変更

--- frontend/src/types/todolist/statics.tsx
+++ frontend/src/types/todolist/statics.tsx
@@ -1,3 +1,9 @@
 export type DueKind = "NONE" | "DATED" | "DATETIME";
 
-export type Priority = "HIGH" | "MIDDLE" | "LOW";
+export const PriorityEnum = {
+    HIGH: "HIGH",
+    MIDDLE: "MIDDLE",
+    LOW: "LOW",
+} as const;
+
+export type Priority = (typeof PriorityEnum)[keyof typeof PriorityEnum];
