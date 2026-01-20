747c8902654bb3d3446ae0f8b1094a43fe1d7699
magu1436 <2cdi1130@cc.u-tokai.ac.jp>
2026-01-20 12:44:49 +0900

    タイムブロックにカスケード削除を追加

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -92,8 +92,12 @@ CREATE TABLE time_blocks (
     width INT NOT NULL,
     start_at TIME,
     color VARCHAR(8) NOT NULL,
-    FOREIGN KEY table_id REFERENCES time_tables(id),
-    FOREIGN KEY schedule_id REFERENCES schedule(id),
+    FOREIGN KEY table_id
+        REFERENCES time_tables(id),
+        ON DELETE CASCADE
+    FOREIGN KEY schedule_id 
+        REFERENCES schedule(id),
+        ON DELETE CASCADE
 
     -- statusがとれる値の制約
     CONSTRAINT chk_status CHECK (status IN ('PLACED', 'HOLD'))
