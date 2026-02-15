c046e193aa6f328b82ca83da0636d501243de27f
magu1436 <hfzoro77@gmail.com>
2026-02-15 23:50:09 +0900

    各オブジェクトに対するマッパー関数を実装

---
+++ frontend/src/feature/timeblocking/mapper/blockMapper.ts
@@ -0,0 +1,39 @@
+import { toScheduleSource } from "@/feature/schesuler/mapper/scheduleMapper";
+import type { TemplateBlockApi, TimeBlockApi } from "../types/api";
+import type { TemplateBlockSource, TimeBlockSource } from "../types/blockSourceTypes";
+
+
+/**
+ * `TimeBlockApi` を `TimeBlockSource` に変換するマッパー関数.
+ * 
+ * @param api APIオブジェクト
+ * @returns 変換されたオブジェクト
+ */
+export const toTimeBlockSource = (api: TimeBlockApi): TimeBlockSource => {
+    return {
+        id: api.id,
+        timeTableId: api.timeTableId,
+        title: api.title,
+        status: api.status,
+        relatedSchedle: api.relatedSchedle && toScheduleSource(api.relatedSchedle),
+        width: api.width,
+        startAt: api.startAt ? new Date(api.startAt) : null,
+        tasks: api.tasks,
+        color: api.color,
+    };
+};
+
+/**
+ * `TemplateBlockApi` を `TemplateBlockSource` に変換するマッパー関数.
+ * 
+ * @param api APIオブジェクト
+ * @returns 変換されたオブジェクト
+ */
+export const toTemplateBlockSource = (api: TemplateBlockApi): TemplateBlockSource => {
+    return {
+        id: api.id,
+        title: api.title,
+        width: api.width,
+        color: api.color,
+    };
+};
