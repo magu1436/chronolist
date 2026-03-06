20d3340b910a71f7e1d956cd4ca5042f71b48f50
konoma1103 <2CDI1103.tokai@gmail.com>
2026-03-07 00:37:40 +0900

    ログインに関するマッパー関数の定義

---
+++ backend/src/main/java/com/magu1436/chronolist/login/mapper/LoginMapper.java
@@ -0,0 +1,10 @@
+package com.magu1436.chronolist.login.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.magu1436.chronolist.login.entity.Users;
+
+@Mapper
+public interface LoginMapper {
+    Users selectByUsername(String username);
+}
