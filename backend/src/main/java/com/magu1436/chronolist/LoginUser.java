a454da13d9aac31c161829be59d27a5f3e182634
milk0924 <sjbt8531zkysyms0pb4w@gmail.com>
2026-03-07 04:45:24 +0900

    feat:UserDetailsの実装

---
+++ backend/src/main/java/com/magu1436/chronolist/LoginUser.java
@@ -0,0 +1,14 @@
+package com.magu1436.chronolist;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+public class LoginUser extends User{
+    public LoginUser(String username,
+        String password,
+        Collection<? extends GrantedAuthority> authorities){
+            super(username, password, authorities);
+    }
+}
