a6856b46633f5ea0487b901304706f01d7e099fb
milk0924 <sjbt8531zkysyms0pb4w@gmail.com>
2026-03-07 11:55:47 +0900

    feat:DB実装に伴うUserDetailsServiceの変更

--- backend/src/main/java/com/magu1436/chronolist/LoginUserDatailsServiceImpl.java
+++ backend/src/main/java/com/magu1436/chronolist/LoginUserDatailsServiceImpl.java
@@ -8,16 +8,23 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LoginUserDatailsServiceImpl implements UserDetailsService{
+
+    private final AuthenticationMapper AuthenticationMapper;
+
     @Override
     public UserDetails loadUserByUsername(String username)
-        throws UsernameNotFoundException{
-            if (username.equals("tarou")){
-                return new LoginUser("tarou",
-                                     "pass",
-                                     Collections.emptyList());
-            } else {
-                throw new UsernameNotFoundException(username + "→ 指定しているユーザー名は存在しません");
-            }
+            throws UsernameNotFoundException{
+
+        Authentication authentication = authenticationMapper.selectByUsername(username);
+
+        if (authentication != null){
+            return new LoginUser(authentication.getUsername(),
+                                    authentication.getPassword(),
+                                    Collections.emptyList());
+        } else {
+            throw new UsernameNotFoundException(username + "→ 指定しているユーザー名は存在しません");
         }
+    }
 }
