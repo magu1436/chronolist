abdfe60472242cb7fc7bdd3382e011b1ffe0b5d0
milk0924 <sjbt8531zkysyms0pb4w@gmail.com>
2026-03-14 12:51:06 +0900

    feat:コントローラーに認可の情報を付与

--- backend/src/main/java/com/magu1436/chronolist/config/SecurityConfig.java
+++ backend/src/main/java/com/magu1436/chronolist/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 // 各ページへのアクセス許可設定
                 .requestMatchers( "/login", "/public/**", "/error", "/").permitAll()
-                .requestMatchers("/general/**").authenticated()
+                .requestMatchers("/general/**", "/api/**").authenticated()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
