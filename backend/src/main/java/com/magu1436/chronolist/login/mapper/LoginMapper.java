00b19ca70ecb0d417dbc674d401081a0766869f4
konoma1103 <2CDI1103.tokai@gmail.com>
2026-03-24 13:04:52 +0900

    マッパー関数`insertUser`を定義

--- backend/src/main/java/com/magu1436/chronolist/login/mapper/LoginMapper.java
+++ backend/src/main/java/com/magu1436/chronolist/login/mapper/LoginMapper.java
@@ -6,5 +6,8 @@ import com.magu1436.chronolist.login.entity.Users;
 
 @Mapper
 public interface LoginMapper {
+    // 指定のログインIDをもつユーザー情報を取得する
     Users getUsersByLoginId(String loginId);
+    // 受け取ったユーザー情報をDBに登録する
+    void insertUser(Users user);
 }
