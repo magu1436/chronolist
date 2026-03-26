1f3ee16fa868f047804b36def5e192f85421e2da
magu1436 <2cdi1130@cc.u-tokai.ac.jp>
2026-03-27 02:49:28 +0900

    ダブルクリックで編集可能なテキストコンポーネントを実装

---
+++ frontend/src/components/EditableText.tsx
@@ -0,0 +1,46 @@
+import { Typography, type TypographyVariant } from "@mui/material";
+import { useState, type FC } from "react";
+
+
+type EditableTextProps = {
+    value?: string,
+    variant?: TypographyVariant | "inherit",
+    style?: React.CSSProperties,
+};
+
+const EditableText: FC<EditableTextProps> = ({ value, variant, style }) => {
+    const [ isEditing, setIsEditing ] = useState(false);
+    const [ text, setText ] = useState<string>(value || "");
+
+    const handleFinishEdit = () => {
+        setIsEditing(false);
+    };
+    const handleKeyDown = (event: React.KeyboardEvent) => {
+        const cancelKeys = ["Escape", "Enter"];
+        if (cancelKeys.includes(event.key)) handleFinishEdit();
+    };
+
+    const textNode = (
+        <Typography
+            style={style}
+            variant={variant}
+            onDoubleClick={() => setIsEditing(true)}
+        >{text}</Typography>
+    );
+    const editNode = (
+        <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onKeyDown={(e) => {handleKeyDown(e);}}
+            onBlur={handleFinishEdit}
+        />
+    );
+
+    return (
+        <>
+            {isEditing ? editNode : textNode}
+        </>
+    )
+};
+
+export default EditableText;
