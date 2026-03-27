78e153501a30701923f44f7f42f17cd00c2a17cd
konoma1103 <2CDI1103.tokai@gmail.com>
2026-03-27 12:18:10 +0900

    `ResponseStatusException`のimport文を削除

--- backend/src/main/java/com/magu1436/chronolist/timeblocking/TimeBlockingController.java
+++ backend/src/main/java/com/magu1436/chronolist/timeblocking/TimeBlockingController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.magu1436.chronolist.scheduler.mapper.SchedulerMapper;
 import com.magu1436.chronolist.timeblocking.entity.TemplateBlock;
