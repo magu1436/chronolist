1df4431e795946572de94df2236055579a85011e
magu1436 <hfzoro77@gmail.com>
2026-03-29 00:15:00 +0900

    セッターの型を修正

--- frontend/src/feature/timeblocking/contexts/SelectedTimeBlockId.tsx
+++ frontend/src/feature/timeblocking/contexts/SelectedTimeBlockId.tsx
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 const SelectedTimeBlockId = createContext<{
     selectedTimeBlockId: number | null,
-    setSelectedTimeBlockId: ((block: number | null | ((block: number | null) => void)) => void)
+    setSelectedTimeBlockId: ((block: number | null | ((block: number | null) => number | null)) => void)
 }>({
     selectedTimeBlockId: null,
     setSelectedTimeBlockId: () => { },
