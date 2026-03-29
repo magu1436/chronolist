4448b10dbe9187ad002e379325f2c91c32888f29
magu1436 <2cdi1130@cc.u-tokai.ac.jp>
2026-03-30 02:56:45 +0900

    `NumberField` に `helperText` を記述できる仕様を追加

--- frontend/src/components/NumberField.tsx
+++ frontend/src/components/NumberField.tsx
@@ -22,11 +22,13 @@ export default function NumberField({
   label,
   error,
   size = 'medium',
+  helperText,
   ...other
 }: BaseNumberField.Root.Props & {
   label?: React.ReactNode;
   size?: 'small' | 'medium';
   error?: boolean;
+  helperText?: React.ReactNode;
 }) {
   let id = React.useId();
   if (idProp) {
@@ -106,7 +108,7 @@ export default function NumberField({
         )}
       />
       <FormHelperText sx={{ ml: 0, '&:empty': { mt: 0 } }}>
-        Enter value between 10 and 40
+        {helperText}
       </FormHelperText>
     </BaseNumberField.Root>
   );
