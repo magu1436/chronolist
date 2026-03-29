e059b059a8b206ac42781d02771f42a6fc9712f8
magu1436 <2cdi1130@cc.u-tokai.ac.jp>
2026-03-29 21:58:30 +0900

    `TimeBlockDetailPanel` のタイトルのみ実装

--- frontend/src/feature/timeblocking/components/TimeBlockDetailPanel.tsx
+++ frontend/src/feature/timeblocking/components/TimeBlockDetailPanel.tsx
@@ -1,16 +1,25 @@
-import { Dialog, Stack } from "@mui/material"
+import { Modal, Stack, Box, type SxProps } from "@mui/material"
 import Title from "./timeBlockDetailPanelComponents/Title"
 import { useCallback, useContext, useEffect, useState, type FC } from "react";
 import SelectedTimeBlockClientId from "../contexts/SelectedTimeBlockClientId";
 import BlockRepositories from "../contexts/BlockRepositories";
+import type { TimeBlockSource } from "../types/blockSourceTypes";
 
-
-type TimeBlockDetailPanelProps = {
-    open: boolean,
-    setOpen: (open: boolean | ((prevState: boolean) => boolean)) => void,
-};
+const style: SxProps = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+}
 
 const TimeBlockDetailPanel: FC = () => {
+
+    const [ block, setBlock ] = useState<TimeBlockSource | undefined>(undefined);
 
     const {
         blocksOnTable,
@@ -18,6 +27,17 @@ const TimeBlockDetailPanel: FC = () => {
         blocksAtField,
         setBlocksAtField,
     } = useContext(BlockRepositories);
+
+    const reflectBlockToRepository = useCallback((block: TimeBlockSource) => {
+        switch (block.status) {
+            case "PLACED":
+                setBlocksOnTable((blocks) => blocks.map(b => b.clientId === block.clientId ? block : b));
+                break;
+            case "HOLD":
+                setBlocksAtField((blocks) => blocks.map(b => b.clientId === block.clientId ? block : b));
+                break;
+        }
+    }, []);
 
     const {
         selectedTimeBlockId,
@@ -28,28 +48,33 @@ const TimeBlockDetailPanel: FC = () => {
 
     useEffect(() => {
         setOpen(selectedTimeBlockId !== null);
+        const selectedBlock = blocksOnTable.find(b => b.clientId === selectedTimeBlockId) || blocksAtField.find(b => b.clientId === selectedTimeBlockId);
+        setBlock(selectedBlock);
     }, [selectedTimeBlockId]);
 
     const handleClose = useCallback(() => {
         setSelectedTimeBlockClientId(null);
     }, []);
 
-    const block = blocksOnTable.find(b => b.clientId === selectedTimeBlockId) || blocksAtField.find(b => b.clientId === selectedTimeBlockId);
-    const [ title, setTitle ] = useState(block?.title || "");
+    const handleSetText = useCallback((text: string) => {
+        const editedBlock = block && { ...block, title: text };
+        setBlock(editedBlock);
+        editedBlock && reflectBlockToRepository(editedBlock);
+    }, [block]);
 
 
     return (
-        <Dialog
+        <Modal
             open={open}
             onClose={handleClose} 
-            sx={{
-                height: "100%",
-            }
-        }>
-            <Stack>
-                <Title text={title} setText={setTitle} />
-            </Stack>
-        </Dialog>
+        >
+            <Box
+                sx={style}>
+                <Stack>
+                    <Title text={block?.title} setText={handleSetText} />
+                </Stack>
+            </Box>
+        </Modal>
     )
 };
 
