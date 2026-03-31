a45a3c58a0168ba3dadfd10fe45e7b18130dcc36
milk0924 <sjbt8531zkysyms0pb4w@gmail.com>
2026-03-31 12:05:16 +0900

    CORS設定のURLの変更

--- backend/src/main/java/com/magu1436/chronolist/login/config/SecurityConfig.java
+++ backend/src/main/java/com/magu1436/chronolist/login/config/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig {
         configuration.setAllowCredentials(true);
         
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/api/**", configuration);
         return source;
     }
 
