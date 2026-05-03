908f7070103eddbbe7b6473f24519b482ae2f834
magu1436 <2cdi1130@cc.u-tokai.ac.jp>
2026-05-03 17:00:04 +0900

    テンプレートブロックをDBに登録する機能を実装

--- frontend/src/feature/timeblocking/api/templateBlock/register.ts
+++ frontend/src/feature/timeblocking/api/templateBlock/register.ts
@@ -3,27 +3,24 @@ import type { TemplateBlockSource } from "../../types/blockSourceTypes";
 
 
 const register = async (block: TemplateBlockSource) => {
-    // const data = {
-    //     title: block.title,
-    //     width: block.width,
-    //     color: block.color,
-    // };
+    const data = {
+        title: block.title,
+        width: block.width,
+        color: block.color,
+    };
 
-    // try {
-    //     const res = await customizedFetch<number>(
-    //         {
-    //             url: "/timeblocking/templateBlock/register",
-    //             method: "PUT",
-    //             data,
-    //         }
-    //     )
-    //     console.log("new id: ", res);
-    //     return res;
-    // } catch (error) {
-    //     throw error;
-    // }
-    console.log("TemplateBlock was registered.");
-    return -1;
+    try {
+        const res = await customizedFetch<number>(
+            {
+                url: "/timeblocking/templateBlock/register",
+                method: "PUT",
+                data,
+            }
+        );
+        return res;
+    } catch (error) {
+        throw error;
+    }
 }
 
 export default register;
