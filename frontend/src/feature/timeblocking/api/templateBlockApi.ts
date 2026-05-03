d229fa7d8beab3c7ab84883b9e8a5cbf533dd609
magu1436 <2cdi1130@cc.u-tokai.ac.jp>
2026-05-03 17:04:13 +0900

    フロント側のテンプレートブロック登録APIを実装

--- frontend/src/feature/timeblocking/api/templateBlockApi.ts
+++ frontend/src/feature/timeblocking/api/templateBlockApi.ts
@@ -1,4 +1,5 @@
 import register from "./templateBlock/register";
 import getAll from "./templateBlock/getAll";
+import update from "./templateBlock/update";
 
-export { register, getAll };
+export { register, getAll, update };
