d0d1b3448377a1de17ae18a6143ff49b648fc565
magu1436 <2cdi1130@cc.u-tokai.ac.jp>
2026-05-04 19:22:41 +0900

    サイドパネルのヘッダーを実装

--- frontend/src/feature/schesuler/components/SidePanel/header.tsx
+++ frontend/src/feature/schesuler/components/SidePanel/header.tsx
@@ -1,6 +1,37 @@
+import type { FC } from "react"
+import { Button, Stack } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
+import EditableText from "@/components/EditableText";
 
 type SidePanelHeaderProps = {
-    schedule: ScheduleSource,
+    title: string,
+    setTitle: (title: string) => void,
     onDelete: () => void,
 }
+
+const DeleteButton = ({ onDelete }: { onDelete: () => void }) => {
+    return (
+        <Button onClick={onDelete} color="error" startIcon={<DeleteIcon />}>削除</Button>
+    )
+}
+
+const Header: FC<SidePanelHeaderProps> = ({ title, setTitle, onDelete }) => {
+    
+    return (
+        <>
+            <Stack
+                direction={"column"}
+                spacing={1}
+            >
+                <Stack direction={"row"} sx={{justifyContent: "right"}}>
+                    <EditableText value={title} onChange={setTitle} variant="h5" />
+                </Stack>
+                <DeleteButton onDelete={onDelete} />
+            </Stack>
+                
+        </>
+    )
+}
+
+export default Header;
