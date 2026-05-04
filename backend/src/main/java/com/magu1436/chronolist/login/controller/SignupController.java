9f1d0a9a9086523184f3638e3f3dd625e0cc103d
milk0924 <sjbt8531zkysyms0pb4w@gmail.com>
2026-05-04 23:26:25 +0900

    SignupControllerの実装

---
+++ backend/src/main/java/com/magu1436/chronolist/login/controller/SignupController.java
@@ -0,0 +1,34 @@
+package com.magu1436.chronolist.login.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.magu1436.chronolist.login.service.SignupService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class SignupController {
+
+    private final SignupService signupService;
+
+    /**
+     * 新規ユーザーを登録する
+     * @param loginId 登録したいログインID
+     * @param password 登録したいパスワード
+     * @return 正常終了時 200 OK
+     * @author milk0924
+     */
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody String loginId, String password){
+        signupService.register(loginId, password);
+        return ResponseEntity.ok().build();
+    }
+    
+}
