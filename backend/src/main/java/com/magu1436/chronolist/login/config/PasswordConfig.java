fd1ed73cbf174eb54ee48fe500ac77f6a971988f
milk0924 <sjbt8531zkysyms0pb4w@gmail.com>
2026-05-04 23:10:03 +0900

    passwordencoderをNoOpPasswordEncoderからBCryptPasswordEncoderに変更

--- backend/src/main/java/com/magu1436/chronolist/login/config/PasswordConfig.java
+++ backend/src/main/java/com/magu1436/chronolist/login/config/PasswordConfig.java
@@ -2,13 +2,13 @@ package com.magu1436.chronolist.login.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordConfig {
     @Bean
     public PasswordEncoder passwordEnsorder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
